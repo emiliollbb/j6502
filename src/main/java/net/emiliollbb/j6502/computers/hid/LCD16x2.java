ed2f007cd2bf9e59db470d1571336bb7e2827c44
emiliollbb <emiliollbb@gmail.com>
2026-04-17 15:38:05 +0200

    LCD

--- src/main/java/net/emiliollbb/j6502/computers/hid/LCD16x2.java
+++ src/main/java/net/emiliollbb/j6502/computers/hid/LCD16x2.java
@@ -11,23 +11,47 @@ public class LCD16x2 extends Canvas {
 	
 	public LCD16x2() {
 		characters = new Character[32];
+		int x=30;
+		int y=30;
+		for(int i=0; i<characters.length; i++) {
+			characters[i]=new Character(x,y);
+			x+=32;
+			if(i==15) {
+				y+=49;
+				x=30;
+			}
+		}
 	}
 	
 	@Override
     public void paint(Graphics g) {
 		g.setColor(Color.decode("#87ad34"));
-		g.fillRect(0, 0, 600, 150);
+		g.fillRect(0, 0, 569, 186);
+		for(int i=0; i<characters.length; i++) {
+			characters[i].paint(g);
+		}
 	}
 	
 	public int getWidth() {
-		return 600;
+		return 569;
 	}
 	
 	public int getHeight() {
-		return 150;
+		return 186;
 	}
 	
 	private class Character {
+		private int x;
+		private int y;
 		
+		public Character(int x, int y) {
+			this.x=x;
+			this.y=y;
+		}
+		
+		public void paint(Graphics g) {
+			g.setColor(Color.decode("#7b9d31"));
+			g.fillRect(x, y, 29, 47);
+		}
 	}
 }
