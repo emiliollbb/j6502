13a5e0cc3f053d7959c8b3c647e5e53ca22bd84d
emiliollbb <emiliollbb@gmail.com>
2026-04-22 15:58:23 +0200

    Screen new palette

---
+++ src/main/java/net/emiliollbb/j6502/computers/hid/ConsoleOutput.java
@@ -0,0 +1,22 @@
+package net.emiliollbb.j6502.computers.hid;
+
+import net.emiliollbb.j6502.chips.AbstractBusDevice;
+
+public class ConsoleOutput extends AbstractBusDevice {
+	
+	public ConsoleOutput(int startAddress) {
+		super(startAddress, 1);
+	}
+
+	@Override
+	protected byte ioRead(int addr) {
+		return 0;
+	}
+
+	@Override
+	protected void ioWrite(int addr, byte data) {
+		System.out.println(String.format("0x%02X", data));
+	}
+
+	
+}
