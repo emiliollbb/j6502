e4f30490283a8d8915aa39f7acea3bee4e60dc74
emiliollbb <emiliollbb@gmail.com>
2026-04-24 14:02:25 +0200

    Tests

--- src/test/java/net/emiliollbb/j6502/chips/Cpu6502Test.java
+++ src/test/java/net/emiliollbb/j6502/chips/Cpu6502Test.java
@@ -25,11 +25,12 @@ public class Cpu6502Test {
 		cpu = new Cpu6502(0, Arrays.asList(device));
 		cpu.setVerbose(10);
 		Mockito.when(device.isInRange(Mockito.anyInt())).thenReturn(true);
-		Mockito.when(device.peek(0xFFFC)).thenReturn((byte)0x00);
-		Mockito.when(device.peek(0xFFFD)).thenReturn((byte)0x02);
 	}
 	
-	private void setProgram(int[] data) {
+	private void loadProgram(int begin, int[] data) {
+		//pc = peek(0xFFFC) | peek(0xFFFD)<<8 & 0x0000FFFF;
+		Mockito.when(device.peek(0xFFFC)).thenReturn((byte)(begin & 0x0000FFFF));
+		Mockito.when(device.peek(0xFFFD)).thenReturn((byte)(begin >>8 & 0x0000FFFF));
 		for(int i=0; i<data.length; i++) {
 			Mockito.when(device.peek(0x0200+i)).thenReturn((byte)data[i]);
 		}
@@ -37,7 +38,7 @@ public class Cpu6502Test {
 	
 	@Test
 	void testLDX() {
-		setProgram(new int[] {0xA2, 0x55});
+		loadProgram(0x200, new int[] {0xA2, 0x55});
 		cpu.reset();
 		int cycles = cpu.step();
 		Assertions.assertEquals(0x55, cpu.getX());
@@ -46,6 +47,8 @@ public class Cpu6502Test {
 	
 	@Test
 	void testBRA() {
+		Mockito.when(device.peek(0xFFFC)).thenReturn((byte)0x00);
+		Mockito.when(device.peek(0xFFFD)).thenReturn((byte)0x02);
 		Mockito.when(device.peek(0x0200)).thenReturn((byte)0x80);
 		Mockito.when(device.peek(0x0201)).thenReturn((byte)0x01);
 		cpu.reset();
